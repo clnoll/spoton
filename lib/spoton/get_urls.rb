83adb222c7c264b30a318180a58531f7dafaf8bc
clnoll <noll.catherine@gmail.com>
2014-09-11 12:40:10 -0700

    array of hrefs for eventbrite

--- lib/spoton/get_urls.rb
+++ lib/spoton/get_urls.rb
@@ -8,14 +8,22 @@ class SpotOn::GetUrls
     @str = str
     # @non_word = non_word
     @urls = []
+    @events = {}
   end
 
   def get_urls
     uri = URI.parse(@str)
-    get_host = uri.host
-    base = URI.parse("http://#{get_host}")
+    host = uri.host
+    parse_str = @str.split('/')
+    base = URI.parse("http://#{host}")
     doc = Nokogiri::HTML(open(base))
-    doc.css('a').each {|i| @urls << i['href'] }
+    doc.css('a').each  do |i|
+        split_event_url = i['href'].split('/')
+        if split_event_url[0..2] == parse_str[0..2] && i['href'] != "http://#{host}"
+            @urls << split_event_url
+        end
+    end
     @urls
+    # parse_str
   end
 end
